bb8d2d96e7e1b9e6fc8a4a22918d145e0fbb0647
sofasurfer <kilian@sofasurfer.ch>
2019-12-06 11:58:50 +0100

    Add 404 page

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_template_part('templates/header');
     <div class="row">
         <div class="col-md-12">
             <figure class="c-ratiobox c-ratiobox-1by1 c-img-circle">
-                <img src="https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" alt="" />
+                <img class="img-responsive" src="https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" alt="" />
             </figure>
         </div>
     </div>
