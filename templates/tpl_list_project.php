5e381ed76fd5a9dff7cdad10313d0f2633f1d463
kbohnenblust <kbohnenblust@raptus.com>
2019-12-19 00:39:05 +0100

    Few responsive changes and fix spaces

--- templates/tpl_list_project.php
+++ templates/tpl_list_project.php
@@ -1,4 +1,4 @@
-<div class="col-md-4 item-hover">
+<div class="col-md-6 item-hover">
     <a class="" title="" href="<?= get_the_excerpt($project); ?>" target="_blank">
         <?= do_shortcode("[render_imagetag size=\"people\" class=\"img-rounded img-responsive\" id=\"".get_post_thumbnail_id($project)."\"]"); ?>
         <div class="caption item-hover-caption" style="display: none;">
