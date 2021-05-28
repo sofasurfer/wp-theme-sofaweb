ff9a2fb5925e585e9bc910219a5e6201ed4f3ac3
sofasurfer <kilian@sofasurfer.ch>
2021-05-28 13:53:14 +0200

    Add page icons

--- templates/tpl_list_page.php
+++ templates/tpl_list_page.php
@@ -1,6 +1,18 @@
+<?php
+$iconname = "fa-briefcase";
+switch ($project->post_name) {
+    case 'about':
+        $iconname = "fa-envelope";
+        break;
+    case 'blog':
+        $iconname = "fa-book";
+        break;
+}
+
+?>
 <div class="col-md-4">
         <a class="" title="" href="<?= get_permalink($project); ?>">     
-            <h3><i class=" fa fa-briefcase "></i> <?= $project->post_title; ?></h3>
+            <h3><i class=" fa <?=$iconname;?> "></i> <?= $project->post_title; ?></h3>
         </a>
         <div class="caption caption-fixed"><?= get_the_excerpt($project); ?></div>  
 </div>
